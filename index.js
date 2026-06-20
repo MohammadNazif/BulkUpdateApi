8d758d9060c973684b5b95a2a654540f2ac44097
Mohammad Nazif <rockingnazif@gmail.com>
2026-06-20 15:12:52 +0530

    Some changes

--- index.js
+++ index.js
@@ -23,51 +23,8 @@ app.post('/api/voters/bulk-update', async (req, res) => {
 
     const pool = await poolPromise;
 
-    // Map incoming format to our database schema
-    const transformedData = data.map(item => {
-      const transformed = {
-        id: item.id,
-        voterNo: item.voterNo,
-        epic: item.epic,
-        name: item.name,
-        houseNo: item.houseNo,
-        age: item.age,
-        gender: item.gender
-      };
-
-      // Handle relation mapping
-      if (item.relationType && item.relationName) {
-        transformed.hasRelation = 1;
-        const rType = item.relationType.toUpperCase();
-        if (rType === 'HUSBAND') {
-          transformed.husbandName = item.relationName;
-        } else if (rType === 'FATHER') {
-          transformed.fatherName = item.relationName;
-        } else if (rType === 'MOTHER') {
-          transformed.motherName = item.relationName;
-        } else {
-          transformed.othersParents = item.relationName;
-        }
-      } else {
-        transformed.hasRelation = 0;
-      }
-
-      // Handle confidence (convert 0-1 float to 0-100 INT)
-      if (typeof item.ocrConfidence === 'number') {
-        transformed.confidence = Math.round(item.ocrConfidence * 100);
-      }
-
-      // Handle needsEvaluation (map to NeedReview)
-      if (typeof item.needsEvaluation !== 'undefined') {
-        transformed.NeedReview = item.needsEvaluation ? 1 : 0;
-      }
-
-      return transformed;
-    });
-
     // Convert array to JSON string for OPENJSON parsing in SQL Server
-    const jsonData = JSON.stringify(transformedData);
-
+    const jsonData = JSON.stringify(data);
     // Using OPENJSON to perform a highly efficient bulk update
     // We update 't' from 'j' by matching on 'id'
     // ISNULL is used so that if a property is not provided in the payload, 
@@ -78,32 +35,35 @@ app.post('/api/voters/bulk-update', async (req, res) => {
         t.voterNo = ISNULL(j.voterNo, t.voterNo),
         t.epic = ISNULL(j.epic, t.epic),
         t.name = ISNULL(j.name, t.name),
-        t.fatherName = CASE WHEN j.hasRelation = 1 THEN j.fatherName ELSE ISNULL(j.fatherName, t.fatherName) END,
-        t.husbandName = CASE WHEN j.hasRelation = 1 THEN j.husbandName ELSE ISNULL(j.husbandName, t.husbandName) END,
-        t.motherName = CASE WHEN j.hasRelation = 1 THEN j.motherName ELSE ISNULL(j.motherName, t.motherName) END,
-        t.othersParents = CASE WHEN j.hasRelation = 1 THEN j.othersParents ELSE ISNULL(j.othersParents, t.othersParents) END,
+        t.fatherName = CASE WHEN j.relationType IS NOT NULL THEN (CASE WHEN UPPER(j.relationType) = 'FATHER' THEN j.relationName ELSE NULL END) ELSE ISNULL(j.fatherName, t.fatherName) END,
+        t.husbandName = CASE WHEN j.relationType IS NOT NULL THEN (CASE WHEN UPPER(j.relationType) = 'HUSBAND' THEN j.relationName ELSE NULL END) ELSE ISNULL(j.husbandName, t.husbandName) END,
+        t.motherName = CASE WHEN j.relationType IS NOT NULL THEN (CASE WHEN UPPER(j.relationType) = 'MOTHER' THEN j.relationName ELSE NULL END) ELSE ISNULL(j.motherName, t.motherName) END,
+        t.othersParents = CASE WHEN j.relationType IS NOT NULL THEN (CASE WHEN UPPER(j.relationType) NOT IN ('FATHER', 'HUSBAND', 'MOTHER') THEN j.relationName ELSE NULL END) ELSE ISNULL(j.othersParents, t.othersParents) END,
         t.houseNo = ISNULL(j.houseNo, t.houseNo),
         t.age = ISNULL(j.age, t.age),
         t.gender = ISNULL(j.gender, t.gender),
-        t.confidence = ISNULL(j.confidence, t.confidence),
-        t.NeedReview = ISNULL(j.NeedReview, t.NeedReview)
+        t.confidence = ISNULL(CAST(ROUND(j.ocrConfidence * 100, 0) AS INT), ISNULL(j.confidence, t.confidence)),
+        t.NeedReview = ISNULL(j.needsEvaluation, ISNULL(j.NeedReview, t.NeedReview))
       FROM [tbl_TotalVoters] t
       INNER JOIN OPENJSON(@jsonData)
       WITH (
-          id NVARCHAR(255),
-          voterNo NVARCHAR(100),
-          epic NVARCHAR(100),
-          name NVARCHAR(255),
-          fatherName NVARCHAR(255),
-          husbandName NVARCHAR(255),
-          motherName NVARCHAR(255),
-          othersParents NVARCHAR(255),
-          houseNo NVARCHAR(100),
-          age INT,
-          confidence INT,
-          NeedReview BIT,
-          gender NVARCHAR(50),
-          hasRelation INT
+          id NVARCHAR(255) '$.id',
+          voterNo NVARCHAR(100) '$.voterNo',
+          epic NVARCHAR(100) '$.epic',
+          name NVARCHAR(255) '$.name',
+          fatherName NVARCHAR(255) '$.fatherName',
+          husbandName NVARCHAR(255) '$.husbandName',
+          motherName NVARCHAR(255) '$.motherName',
+          othersParents NVARCHAR(255) '$.othersParents',
+          houseNo NVARCHAR(100) '$.houseNo',
+          age INT '$.age',
+          ocrConfidence FLOAT '$.ocrConfidence',
+          confidence INT '$.confidence',
+          needsEvaluation BIT '$.needsEvaluation',
+          NeedReview BIT '$.NeedReview',
+          gender NVARCHAR(50) '$.gender',
+          relationType NVARCHAR(50) '$.relationType',
+          relationName NVARCHAR(255) '$.relationName'
       ) j ON t.id = j.id
     `;
 
